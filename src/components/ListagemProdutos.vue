95782e999c0133f3a7f6f7bf3d9c3f5da3c3ebce
kvernert <kverner47@gmail.com>
2024-03-05 16:55:24 -0300

    Criação Composables

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import { useScreen } from '@/composables/screen';
 const produtos = ref([]);
 
 onMounted(async () => {
@@ -9,12 +10,22 @@ onMounted(async () => {
 });
 
 const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
+
+const { browserWidth, deviceWidth, isMobile } = useScreen();
+
 </script>
 
 
 <template>
   <div>
-    <h1>Produtos</h1>
+  <div>
+    <h1>
+       Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{
+      isMobile}} 
+      <span v-if="isMobile">É móvel</span>
+    </h1>
+    ...
+  </div>
     <div class="container">
       <div class="card" v-for="produto in produtos" :key="produto.id">
         <h1 class="card--title">{{ produto.title }}</h1>
